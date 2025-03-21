f8df3cea8980fc91a4092fa9e4e8501715c4fc27
Alex Gubin <a.v.gubin0911@gmail.com>
2025-03-21 22:46:25 +0500

    feat: use static

---
+++ src/routes/+layout.js
@@ -0,0 +1,5 @@
+// This disables SSR for all routes
+export const ssr = false;
+
+// This makes SvelteKit load the fallback page in SPA mode
+export const prerender = true;
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +12,12 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// See https://kit.svelte.dev/docs/adapter-vercel for options
-			runtime: 'nodejs18.x',
-			split: false
+			// See https://kit.svelte.dev/docs/adapter-static for options
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
 		}),
 		alias: {
 			$lib: './src/lib'
